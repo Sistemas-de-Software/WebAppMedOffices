e03ba88d528a6903d2b427df71a526054b1a5639
ipadilla <padilla.carlos15@gmail.com>
2020-09-28 21:49:33 -0300

    Carga de Datos

--- WebAppMedOffices/Migrations/Configuration.cs
+++ WebAppMedOffices/Migrations/Configuration.cs
@@ -4,6 +4,7 @@ namespace WebAppMedOffices.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using WebAppMedOffices.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<WebAppMedOffices.Models.ApplicationDbContext>
     {
@@ -19,6 +20,67 @@ namespace WebAppMedOffices.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+            context.Consultorios.AddOrUpdate(x => x.Id,
+                new Consultorio() { Id = 1, Nombre = "Sala 1" },
+                new Consultorio() { Id = 2, Nombre = "Sala 2" },
+                new Consultorio() { Id = 3, Nombre = "Sala 3" }
+                );
+            context.Medicos.AddOrUpdate(x => x.Id,
+                new Medico() { Id = 1, Nombre = "Roberto", Apellido = "Suarez", Telefono = "443285", Celular = "1160848392", Matricula = "12345" },
+                new Medico() { Id = 2, Nombre = "Juan", Apellido = "Sanchez", Telefono = "446280", Celular = "1180828592", Matricula = "23456" },
+                new Medico() { Id = 3, Nombre = "Kevin", Apellido = "Ferreyra", Telefono = "442133", Celular = "1130048955", Matricula = "65321" }
+                );
+            context.AtencionHorarios.AddOrUpdate(x => x.Id,
+                new AtencionHorario() { Id = 1, ConsultorioId = 1, MedicoId = 1, TrabajoTurno = Shared.TrabajoTurno.M, Dia = Shared.Dia.LUN, HoraInicio = new DateTime(2020, 09, 01, 10, 00, 0), HoraFin = new DateTime(2020, 09, 01, 10, 20, 0) },
+                new AtencionHorario() { Id = 2, ConsultorioId = 1, MedicoId = 1, TrabajoTurno = Shared.TrabajoTurno.M, Dia = Shared.Dia.LUN, HoraInicio = new DateTime(2020, 09, 01, 10, 20, 0), HoraFin = new DateTime(2020, 09, 01, 10, 40, 0) },
+                new AtencionHorario() { Id = 3, ConsultorioId = 1, MedicoId = 1, TrabajoTurno = Shared.TrabajoTurno.M, Dia = Shared.Dia.LUN, HoraInicio = new DateTime(2020, 09, 01, 10, 40, 0), HoraFin = new DateTime(2020, 09, 01, 11, 00, 0) }
+                );
+
+            context.Especialidades.AddOrUpdate(x => x.Id,
+                new Especialidad() { Id = 1, Nombre = "Radiologia" },
+                new Especialidad() { Id = 2, Nombre = "Oftalmologia" },
+                new Especialidad() { Id = 3, Nombre = "Traumatologia" }
+                );
+            context.DuracionTurnoEspecialidades.AddOrUpdate(x => x.Id,
+                new DuracionTurnoEspecialidad() { Id = 1, MedicoId = 1, EspecialidadId = 1, Duracion = 20 },
+                new DuracionTurnoEspecialidad() { Id = 2, MedicoId = 2, EspecialidadId = 2, Duracion = 20 },
+                new DuracionTurnoEspecialidad() { Id = 3, MedicoId = 3, EspecialidadId = 3, Duracion = 20 }
+                );
+            context.ObrasSociales.AddOrUpdate(x => x.Id,
+                new ObraSocial() { Id = 1, Nombre = "OsFatun", Telefono = "447800", Email = "osfatun@gmail.com" },
+                new ObraSocial() { Id = 2, Nombre = "OSDE", Telefono = "424500", Email = "osde@gmail.com" },
+                new ObraSocial() { Id = 3, Nombre = "Swiss Medical", Telefono = "423990", Email = "swiss@gmail.com" }
+                );
+            context.ObraSocialTarifas.AddOrUpdate(x => x.Id,
+                new ObraSocialTarifa() { Id = 1, Tarifa = new decimal(400.50), ObraSocialId = 1, EspecialidadId = 1 },
+                new ObraSocialTarifa() { Id = 2, Tarifa = new decimal(500.20), ObraSocialId = 1, EspecialidadId = 2 },
+                new ObraSocialTarifa() { Id = 3, Tarifa = new decimal(700.60), ObraSocialId = 1, EspecialidadId = 3 }
+                );
+            context.TipoEnfermedades.AddOrUpdate(x => x.Id,
+            new TipoEnfermedad() { Id = 1, Nombre = "Cancer de pulmon" },
+            new TipoEnfermedad() { Id = 2, Nombre = "Cancer de piel" },
+            new TipoEnfermedad() { Id = 3, Nombre = "Carcinoma" }
+            );
+            context.Pacientes.AddOrUpdate(x => x.Id,
+            new Paciente() { Id = 1, Nombre = "Franco", Apellido = "Baez", Documento = "40345671", FechaNacimiento = new DateTime(1998, 10, 01), Direccion = "Oribe 892", Telefono = "442342", Celular = "1143234267", NombreContactoEmergencia = "Fulanito1", TelefonoContactoEmergencia = "432580" },
+            new Paciente() { Id = 2, Nombre = "Soledad", Apellido = "Ibarra", Documento = "30428916", FechaNacimiento = new DateTime(1999, 05, 20), Direccion = "Alvear 1213", Telefono = "425154", Celular = "1165423452", NombreContactoEmergencia = "Fulanito2", TelefonoContactoEmergencia = "425432" },
+            new Paciente() { Id = 3, Nombre = "Jesica", Apellido = "Amodil", Documento = "40314424", FechaNacimiento = new DateTime(2000, 01, 01), Direccion = "Atahualpa 703", Telefono = "443285", Celular = "1162808393", NombreContactoEmergencia = "Fulanito3", TelefonoContactoEmergencia = "441242" }
+            );
+            context.Enfermedades.AddOrUpdate(x => x.Id,
+                new Enfermedad() { Id = 1, Nombre = "Cancer", TipoEnfermedadId = 1 }
+                );
+            context.PacienteEnfermedades.AddOrUpdate(x => x.Id,
+            new PacienteEnfermedad() { Id = 1, PacienteId = 1, EnfermedadId = 1, Descripcion = "Enfermedad cronica"}
+            //new PacienteEnfermedad() { Id = 2, PacienteId = 2, EnfermedadId = 2, Descripcion = "Alta colestorol" },
+            //new PacienteEnfermedad() { Id = 3, PacienteId = 3, EnfermedadId = 3, Descripcion = "Falta de respiración" }
+            );
+            /*
+            context.PacienteTurnos.AddOrUpdate(x => x.Id,
+                new PacienteTurno() { Id = 1, PacienteId = 1, TurnoId = 1})
+            
+            context.Turnos.AddOrUpdate(x => x.Id,
+            new Models.Turno() { Id = 1, MedicoId = 1, EspecialidadId = 1, ObraSocialId = 1, Estado = Shared.Estado.Disponible, FechaHora = new DateTime(), Costo = new decimal(), Sobreturno = false, TieneObraSocial = true}    
+            );*/
         }
     }
 }
