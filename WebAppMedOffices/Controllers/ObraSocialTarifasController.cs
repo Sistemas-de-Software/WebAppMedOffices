0abfacd660780e66abc75373cac060e44d8c4c3a
Ernesto Aguaysol <ernestoaguaysol@gmail.com>
2020-09-30 16:53:15 -0300

    Altas de Tarifas

--- WebAppMedOffices/Controllers/ObraSocialTarifasController.cs
+++ WebAppMedOffices/Controllers/ObraSocialTarifasController.cs
@@ -15,14 +15,12 @@ namespace WebAppMedOffices.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: ObraSocialTarifas
         public async Task<ActionResult> Index()
         {
             var obraSocialTarifas = db.ObraSocialTarifas.Include(o => o.Especialidad).Include(o => o.ObraSocial);
             return View(await obraSocialTarifas.ToListAsync());
         }
 
-        // GET: ObraSocialTarifas/Details/5
         public async Task<ActionResult> Details(int? id)
         {
             if (id == null)
@@ -37,7 +35,6 @@ namespace WebAppMedOffices.Controllers
             return View(obraSocialTarifa);
         }
 
-        // GET: ObraSocialTarifas/Create
         public ActionResult Create()
         {
             ViewBag.EspecialidadId = new SelectList(db.Especialidades, "Id", "Nombre");
@@ -45,9 +42,6 @@ namespace WebAppMedOffices.Controllers
             return View();
         }
 
-        // POST: ObraSocialTarifas/Create
-        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
-        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create([Bind(Include = "Id,Tarifa,ObraSocialId,EspecialidadId")] ObraSocialTarifa obraSocialTarifa)
@@ -64,7 +58,6 @@ namespace WebAppMedOffices.Controllers
             return View(obraSocialTarifa);
         }
 
-        // GET: ObraSocialTarifas/Edit/5
         public async Task<ActionResult> Edit(int? id)
         {
             if (id == null)
@@ -81,9 +74,6 @@ namespace WebAppMedOffices.Controllers
             return View(obraSocialTarifa);
         }
 
-        // POST: ObraSocialTarifas/Edit/5
-        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
-        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Edit([Bind(Include = "Id,Tarifa,ObraSocialId,EspecialidadId")] ObraSocialTarifa obraSocialTarifa)
@@ -99,7 +89,6 @@ namespace WebAppMedOffices.Controllers
             return View(obraSocialTarifa);
         }
 
-        // GET: ObraSocialTarifas/Delete/5
         public async Task<ActionResult> Delete(int? id)
         {
             if (id == null)
@@ -114,7 +103,6 @@ namespace WebAppMedOffices.Controllers
             return View(obraSocialTarifa);
         }
 
-        // POST: ObraSocialTarifas/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> DeleteConfirmed(int id)
