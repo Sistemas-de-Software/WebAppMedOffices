d71bb1b119791effbcdc51929289df06606e595e
Ernesto Aguaysol <ernestoaguaysol@gmail.com>
2020-10-04 15:47:41 -0300

    Mensajes genéricos Alert

---
+++ WebAppMedOffices/Constants/Application.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppMedOffices.Constants
+{
+    public class Application
+    {
+        public const string Name = "App MedOffices";
+        public const string ShortName = "MedOffices";
+
+        public const string MessageViewBagName = "Message";
+        public const string AdminRoleName = "Admin";
+    }
+}
---
+++ WebAppMedOffices/Models/GenericMessageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebAppMedOffices.Models
+{
+    [NotMapped]
+    public class GenericMessageViewModel
+    {
+        public GenericMessageViewModel()
+        {
+            MessageType = GenericMessages.info;
+        }
+        public string Message { get; set; }
+
+        public GenericMessages MessageType { get; set; }
+
+        public bool ConstantMessage { get; set; }
+    }
+
+    public enum GenericMessages
+    {
+        warning,
+        danger,
+        success,
+        info
+    }
+}
