68da16989828764c7e68affc5e6a195b9fc95c7f
ipadilla <padilla.carlos15@gmail.com>
2020-10-14 23:12:01 -0300

    En el modelo de paciente se agrega el MAIL

--- WebAppMedOffices/Models/Paciente.cs
+++ WebAppMedOffices/Models/Paciente.cs
@@ -68,6 +68,12 @@ namespace WebAppMedOffices.Models
         [Display(Name = "Teléfono de Emergencia")]
         public string TelefonoContactoEmergencia { get; set; }
 
+        [Required(ErrorMessage = "Debes introducir un {0}")]
+        [StringLength(30, ErrorMessage = "El campo {0} puede contener un máximo de {1} y un mínimo de {2} caracteres", MinimumLength = 3)]        
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "E-mail")]
+        public string Mail { get; set; }
+
         [Display(Name = "Nombre y Apellido")]
         public virtual string NombreCompleto
         {
