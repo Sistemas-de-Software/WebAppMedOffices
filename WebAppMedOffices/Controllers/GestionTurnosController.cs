30285eaf4d50863851957ce20a6c08301676ec88
Damian <Damian@DESKTOP-NJ67DL7>
2020-11-11 00:20:30 -0300

    Req 14 Como secretaria deseo cancelar turno por un medico

--- WebAppMedOffices/Controllers/GestionTurnosController.cs
+++ WebAppMedOffices/Controllers/GestionTurnosController.cs
@@ -1207,7 +1207,48 @@ namespace WebAppMedOffices.Controllers
         }
 
 
-        //Cancelar Turno Damian
+
+        //Cancelar Turno2
+        public async Task<ActionResult> CancelarTurno2(int? id)
+        {
+
+
+            Turno turno = await db.Turnos.FindAsync(id);
+
+            if (id == null)
+            {
+                TempData[Application.MessageViewBagName] = new GenericMessageViewModel
+                {
+                    Message = "No existe el turno.",
+                    MessageType = GenericMessages.warning
+                };
+                return RedirectToAction("Index");
+            }
+
+
+            turno.ObraSocialId = null;
+            turno.PacienteId = null;
+            turno.Estado = Estado.Disponible;
+            turno.Costo = null;
+            turno.Sobreturno = null;
+            turno.TieneObraSocial = null;
+
+
+            db.Entry(turno).State = EntityState.Modified;
+            await db.SaveChangesAsync();
+            TempData[Application.MessageViewBagName] = new GenericMessageViewModel
+            {
+             
+                Message = "Turno cancelado exitosamante.",
+                MessageType = GenericMessages.success
+            };
+
+            return RedirectToAction("ListaPacientes");
+
+        }
+
+
+        //Cancelar sobreTurno
         public async Task<ActionResult> CancelarSobreturno(int? id)
         {
             
