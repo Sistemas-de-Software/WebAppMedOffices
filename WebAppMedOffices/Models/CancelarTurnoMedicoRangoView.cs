e91267ac89c6f369ac0e69478271fdd6e9999dff
Ernesto Aguaysol <ernestoaguaysol@gmail.com>
2020-11-12 15:54:11 -0300

    Cancelar turnos por rango (Elimina el turno)

---
+++ WebAppMedOffices/Models/CancelarTurnoMedicoRangoView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebAppMedOffices.Models
+{
+    [NotMapped]
+    public class CancelarTurnoMedicoRangoView
+    {
+        public int MedicoId { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Fecha Desde")]
+        public DateTime FechaDesde { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Fecha Hasta")]
+        public DateTime FechaHasta { get; set; }
+    }
+}
