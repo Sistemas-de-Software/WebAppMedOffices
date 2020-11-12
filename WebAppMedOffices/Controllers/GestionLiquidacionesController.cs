2ae00a0e98671594c32b01895f77f06c25eac1f1
Ernesto Aguaysol <ernestoaguaysol@gmail.com>
2020-11-11 23:41:53 -0300

    Liquidación paciente Particulares

--- WebAppMedOffices/Controllers/GestionLiquidacionesController.cs
+++ WebAppMedOffices/Controllers/GestionLiquidacionesController.cs
@@ -51,33 +51,52 @@ namespace WebAppMedOffices.Controllers
             }
 
             var hoy = DateTime.Now.Date;
-            //if (liquidacionPacientes.FechaDesde.Date < hoy)
-            //{
-            //    TempData[Application.MessageViewBagName] = new GenericMessageViewModel
-            //    {
-            //        Message = "La fecha Desde no puede ser menor que la fecha de Hoy.",
-            //        MessageType = GenericMessages.warning
-            //    };
-            //    return RedirectToAction("CrearLiquidacionPacientesParticulares");
-            //}
+            if (liquidacionPacientes.FechaHasta.Date > hoy)
+            {
+                TempData[Application.MessageViewBagName] = new GenericMessageViewModel
+                {
+                    Message = "La liquidación puede ser solo hasta la fecha de Hoy.",
+                    MessageType = GenericMessages.warning
+                };
+                return RedirectToAction("CrearLiquidacionPacientesParticulares");
+            }
 
             try
             {
-                var turnos = db.Turnos.Include(t => t.Paciente)
-                                                     .Where(t => t.ObraSocialId == 1 && 
-                                                                t.Estado == Shared.Estado.Atendido &&
-                                                                DbFunctions.TruncateTime(t.FechaHora) >= liquidacionPacientes.FechaDesde &&
-                                                                DbFunctions.TruncateTime(t.FechaHora) <= liquidacionPacientes.FechaHasta)
-                                                     .GroupBy(t => t.PacienteId).
-                                                     Select(g => new LiquidacionPacienteViewModel
-                                                     {
-                                                         PacienteId = g.Key.Value,
-                                                         Turnos = g.ToList(),
-                                                         SubTotal = g.Sum(p => p.Costo.Value),
-                                                         Fecha = hoy
-                                                     });
+                List<Turno> liquidacionesDB = await db.Turnos
+                    .Where(t => t.ObraSocialId == 1 &&
+                        t.Estado == Shared.Estado.Atendido &&
+                        DbFunctions.TruncateTime(t.FechaHora) >= liquidacionPacientes.FechaDesde &&
+                        DbFunctions.TruncateTime(t.FechaHora) <= liquidacionPacientes.FechaHasta).ToListAsync();
 
-                if (turnos.Count() == 0)
+
+                ICollection<Medico> medicos = await db.Medicos.ToListAsync();
+
+                List<LiquidacionViewModel> liquidacionesTotales = new List<LiquidacionViewModel>();
+
+                foreach (var medico in medicos)
+                {
+                    foreach (var duracionTurnoEspecialidad in medico.DuracionTurnoEspecialidades)
+                    {
+                        List<Turno> turnos = new List<Turno>();
+                        turnos = liquidacionesDB.Where(t => t.MedicoId == medico.Id &&
+                            t.EspecialidadId == duracionTurnoEspecialidad.EspecialidadId).ToList();
+
+                        if (turnos.Count() > 0)
+                        {
+                            LiquidacionViewModel liquidacion = new LiquidacionViewModel { 
+                                Medico = medico,
+                                Especialidad = duracionTurnoEspecialidad.Especialidad,
+                                Turnos = turnos,
+                                SubTotal = turnos.Sum(t => t.Costo.Value)
+                            };
+
+                            liquidacionesTotales.Add(liquidacion);
+                        }
+                    }
+                }
+
+                if (liquidacionesTotales.Count() == 0)
                 {
                     TempData[Application.MessageViewBagName] = new GenericMessageViewModel
                     {
@@ -87,9 +106,9 @@ namespace WebAppMedOffices.Controllers
                     return RedirectToAction("CrearLiquidacionPacientesParticulares");
                 }
                 
-                ViewBag.Total = await turnos.SumAsync(t => t.SubTotal);
+                ViewBag.Total = liquidacionesTotales.Sum(t => t.SubTotal);
 
-                return View(await turnos.ToListAsync());
+                return View(liquidacionesTotales);
             }
             catch (Exception ex)
             {
@@ -103,7 +122,6 @@ namespace WebAppMedOffices.Controllers
             }
 
         }
-
 
         [HttpPost]
         [ValidateAntiForgeryToken]
---
+++ WebAppMedOffices/Models/LiquidacionViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebAppMedOffices.Models
+{
+    [NotMapped]
+    public class LiquidacionViewModel
+    {
+        public Medico Medico { get; set; }
+
+        public Especialidad Especialidad { get; set; }
+
+        public ICollection<Turno> Turnos { get; set; }
+
+        public decimal SubTotal { get; set; }
+    }
+}
